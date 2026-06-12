04c473e8a73c9c60e41cb42e4c45c3f55b7cb0c7
Emir <emir@local.com>
2026-06-12 23:05:42 +0300

    Category database tables and category eloquent model

---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'parent_id',
+        'title',
+        'keywords',
+        'description',
+        'image',
+        'status',
+    ];
+}
